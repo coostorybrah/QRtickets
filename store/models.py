dee30564bd17883350f79b78263345329ea1fe85
thao bt <buithanhthao284@gmail.com>
2026-03-24 15:11:45 +0700

    update database

--- store/models.py
+++ store/models.py
@@ -24,3 +24,20 @@ class Product(models.Model):
     
     def get_url(self):
         return reverse("product_detail", args=[self.category.slug, self.slug])
+
+class Variation(models.Model):
+
+    variation_category_choice = (
+        ('price', 'price'),
+    )
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value     = models.CharField(max_length=100)
+    is_active           = models.BooleanField(default=True)
+    created_date        = models.DateTimeField(auto_now=True)
+
+    #objects = VariationManager()
+
+    def __unicode__(self):
+        return self.product
